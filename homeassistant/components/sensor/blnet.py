9210dff81ff3269aef286a480ee1a0f7da391a15
nielstron <n.muendler@web.de>
2018-04-01 17:48:22 +0200

    Code beautification

--- homeassistant/components/sensor/blnet.py
+++ homeassistant/components/sensor/blnet.py
@@ -4,13 +4,7 @@ TODO: as component
 """
 import logging
 
-import voluptuous as vol
-import asyncio
-
-from homeassistant.const import (
-    CONF_RESOURCE, STATE_UNKNOWN, CONF_PASSWORD)
 from homeassistant.helpers.entity import Entity
-import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,26 +18,26 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     if discovery_info is None:
         _LOGGER.error("No BL-Net communication configured")
         return False
-    
-    id = discovery_info['id']
-    blnet_id = discovery_info['ent_id']
-    comm = hass.data[DOMAIN + '_data']
 
-    add_devices([BLNETComponent(hass, id, blnet_id, comm)], True)
+    sensor_id = discovery_info['id']
+    blnet_id = discovery_info['ent_id']
+    comm = hass.data['{}_data'.format(DOMAIN)]
+
+    add_devices([BLNETComponent(hass, sensor_id, blnet_id, comm)], True)
     return True
 
 
 class BLNETComponent(Entity):
     """Implementation of a BL-NET - UVR1611 sensor and switch component."""
 
-    def __init__(self, hass, id, blnet_id, communication):
+    def __init__(self, hass, sensor_id, blnet_id, communication):
         """Initialize the BL-NET sensor."""
-        self.id = id
+        self.id = sensor_id
         self._blnet_id = blnet_id
         self.communication = communication
         self._name = blnet_id
         self._friendly_name = blnet_id
-        self._state = STATE_UNKNOWN
+        self._state = None
         self._unit_of_measurement = None
         self._icon = None
 
@@ -56,17 +50,17 @@ class BLNETComponent(Entity):
     def state(self):
         """Return the state of the device."""
         return self._state
-    
+
     @property
     def icon(self):
         """Return the state of the device."""
         return self._icon
-    
+
     @property
     def unit_of_measurement(self):
         """Return the state of the device."""
         return self._unit_of_measurement
-    
+
     @property
     def device_state_attributes(self):
         """Return the state attributes of the device."""
@@ -74,16 +68,15 @@ class BLNETComponent(Entity):
 
         attrs[FRIENDLY_NAME] = self._friendly_name
         return attrs
-    
+
     def update(self):
         """Get the latest data from communication device """
         sensor_data = self.communication.data.get(self._blnet_id)
-        
+
         if sensor_data is None:
             return
-        
+
         self._friendly_name = sensor_data.get('friendly_name')
         self._state = sensor_data.get('value')
         self._unit_of_measurement = sensor_data.get('unit_of_measurement')
         self._icon = sensor_data.get('icon')
-
