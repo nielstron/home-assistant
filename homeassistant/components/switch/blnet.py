222b762075c4979205a59650bb7a2f08c73e34f3
nielstron <n.muendler@web.de>
2018-08-07 00:50:15 +0200

    Introduce switch for change of operation mode
    A switch with the appendix 'automated' is added, allowing to change
    between manual and automated operation mode

--- homeassistant/components/switch/blnet.py
+++ homeassistant/components/switch/blnet.py
@@ -28,7 +28,8 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     blnet_id = discovery_info['ent_id']
     comm = hass.data['{}_data'.format(DOMAIN)]
 
-    add_devices([BLNETSwitch(switch_id, blnet_id, comm)], True)
+    add_devices([BLNETSwitch(switch_id, blnet_id, comm),
+                 BLNETModeSwitch(switch_id, blnet_id, comm)], True)
     return True
 
 
@@ -44,8 +45,7 @@ class BLNETSwitch(SwitchDevice):
         self.communication = comm
         self._name = blnet_id
         self._friendly_name = blnet_id
-        self._state = False
-        self._state = None
+        self._state = STATE_UNKNOWN
         self._assumed_state = True
         self._icon = None
         self._mode = STATE_UNKNOWN
@@ -114,6 +114,98 @@ class BLNETSwitch(SwitchDevice):
     def turn_off(self, **kwargs):
         """Turn the device off."""
         self.communication.turn_off(self._id)
+        self._state = 'off'
+        self._assumed_state = True
+
+    @property
+    def assumed_state(self)->bool:
+        return self._assumed_state
+
+class BLNETModeSwitch(SwitchDevice):
+    """
+    Representation of a switch that toggles the operation mode
+    of a digital output of the UVR1611. On means automated
+    """
+
+    def __init__(self, switch_id, blnet_id, comm):
+        """Initialize the switch."""
+        self._blnet_id = blnet_id
+        self._id = switch_id
+        self.communication = comm
+        self._name = '{}_automated'.format(blnet_id)
+        self._friendly_name = blnet_id
+        self._state = STATE_UNKNOWN
+        self._assumed_state = True
+        self._icon = None
+        self._mode = STATE_UNKNOWN
+        self._last_updated = None
+
+    def update(self):
+        """Get the latest data from communication device """
+        # check if new data has arrived
+        last_blnet_update = self.communication.last_updated()
+
+        if last_blnet_update == self._last_updated:
+            return
+
+        sensor_data = self.communication.data.get(self._blnet_id)
+
+        if sensor_data is None:
+            return
+
+        self._friendly_name = "{} automated".format(
+            sensor_data.get('friendly_name'))
+        if sensor_data.get('mode') == 'HAND':
+            self._state = 'off'
+            self._icon = 'mdi:hand'
+        else:
+            self._state = 'on'
+            self._icon = 'mdi:settings'
+        
+        self._activation_state = sensor_data.get('value')
+
+        self._last_updated = last_blnet_update
+        self._assumed_state = False
+
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Return the state of the device."""
+        return self._icon
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the device."""
+        attrs = {}
+        attrs[FRIENDLY_NAME] = self._friendly_name
+        return attrs
+
+    @property
+    def is_on(self):
+        """Return true if device is on."""
+        return self._state
+
+    def turn_on(self, **kwargs):
+        """Turn the device on."""
+        self.communication.turn_auto(self._id)
+        self._state = 'on'
+        self._assumed_state = True
+
+    def turn_off(self, **kwargs):
+        """Turn the device off."""
+        if self._activation_state == 'on':
+            self.communication.turn_on(self._id)
+        else:
+            self.communication.turn_off(self._id)
         self._state = 'off'
         self._assumed_state = True
 
